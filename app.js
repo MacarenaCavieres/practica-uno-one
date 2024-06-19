8079d26055536d12be715d6f38571726628a02d4
MCavieres <maca.c.91@gmail.com>
2024-06-19 17:46:22 -0400

    incorporacion de contador

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const secretNumb = Math.floor(Math.random() * 10 + 1);
 let userNumb = +prompt("Elija un número del 1 al 10");
 
+let contador = 1;
+
 while (userNumb !== secretNumb) {
     if (secretNumb > userNumb) {
         alert("Skynet eligio un número mayor");
@@ -8,4 +10,18 @@ while (userNumb !== secretNumb) {
         alert("Skynet eligio un número menor");
     }
     userNumb = +prompt("Elija un número del 1 al 10");
+    contador++;
 }
+
+alert("Te tomo " + contador + " intentos acertar");
+
+// const day = prompt("Que dia de semana es?").toLowerCase();
+
+// if (day === "sabado" || day === "domingo") {
+//     alert("Buen fin de semana");
+// } else {
+//     alert("Buena semana");
+// }
+
+// const nameUser = prompt("Escribe tu nombre");
+// alert(`Bienvenid@ ${nameUser}`);
